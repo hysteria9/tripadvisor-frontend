a49390c7b52e26cf8956e83020a63476c6940581
hysteria9 <anthonyrov@gmail.com>
2020-12-12 20:36:35 +0100

    first commit

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const $ = document;
+
+$.addEventListener("DOMContentLoaded", () => {
+  console.log("Page successfully loaded");
+
+  $.querySelector("#login-btn").addEventListener("click", () => {
+    $.querySelector("#signup-modal").classList.toggle("signup-modal");
+  });
+
+  $.querySelector("#signup-close-modal").addEventListener("click", () => {
+    $.querySelector("#signup-modal").classList.toggle("signup-modal");
+  });
+
+  $.querySelector("#signup2login").addEventListener("click", () => {
+    $.querySelector("#signup-modal").classList.toggle("signup-modal");
+    $.querySelector("#login-modal").classList.toggle("login-modal");
+  });
+
+  $.querySelector("#login-close-modal").addEventListener("click", () => {
+    $.querySelector("#login-modal").classList.toggle("login-modal");
+  });
+
+  $.querySelector("#signup-form").addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    const newUserData = {
+      firstname: $.querySelector("#firstname").value,
+      lastname: $.querySelector("#lastname").value,
+      email: $.querySelector("#signup-email").value,
+      password: $.querySelector("#signup-password").value,
+      description: $.querySelector("#description").value,
+    };
+    console.log(newUserData);
+
+    const response = await axios.post(
+      "http://localhost:3000/signup-form",
+      newUserData
+    );
+    console.log(response);
+  });
+
+  $.querySelector("#login-form").addEventListener("click", (event) => {
+    event.preventDefault();
+  });
+});
